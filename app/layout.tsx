7d61099e521cd82924df636a8a415c70dc6ccdd2
Jaime Pascual Torres <jpascual@everlyrusher.com>
2025-06-01 16:46:52 +0200

    added simpleanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,8 @@ export default function RootLayout({
         <Navbar />
         <main className="min-h-screen">{children}</main>
         <Footer />
+        <!-- 100% privacy-first analytics -->
+        <script async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
       </body>
     </html>
   )
