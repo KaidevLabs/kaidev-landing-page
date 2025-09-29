c73de860acb04352b2d8ebb1926f5e6d40f37ca3
Jaime Pascual Torres <jpascual@everlyrusher.com>
2025-09-29 17:33:01 +0200

    Improved footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import { Github, Linkedin, Twitter } from "lucide-react"
+import { Github, Linkedin, Mail, Twitter } from "lucide-react"
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
@@ -23,12 +23,12 @@ const Footer = () => {
               Consultancy specializing in software development and code refactoring, inspired by the Japanese philosophy
               of Kaizen - continuous improvement.
             </p>
-            <div className="flex space-x-4">
+            <div className="flex space-x-4 inline-block align-middle">
               <a
                 href="https://github.com/EverlyRusher"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors"
+                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors flex justify-center items-center"
                 aria-label="GitHub"
               >
                 <Github className="h-5 w-5" />
@@ -37,7 +37,7 @@ const Footer = () => {
                 href="https://www.linkedin.com/company/kaidev/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors"
+                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors flex justify-center items-center"
                 aria-label="LinkedIn"
               >
                 <Linkedin className="h-5 w-5" />
@@ -46,12 +46,21 @@ const Footer = () => {
                 href="https://bsky.app/profile/jpascual.kaidev.io"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors"
+                className="text-warmWhite/70 hover:text-kaidevTeal transition-colors flex justify-center items-center"
                 aria-label="BlueSky"
               >
                 <svg className="h-5 w-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
                   <path d="M288 227.1C261.9 176.4 190.9 81.9 124.9 35.3 61.6-9.4 37.5-1.7 21.6 5.5 3.3 13.8 0 41.9 0 58.4S9.1 194 15 213.9c19.5 65.7 89.1 87.9 153.2 80.7 3.3-.5 6.6-.9 10-1.4-3.3 .5-6.6 1-10 1.4-93.9 14-177.3 48.2-67.9 169.9 120.3 124.6 164.8-26.7 187.7-103.4 22.9 76.7 49.2 222.5 185.6 103.4 102.4-103.4 28.1-156-65.8-169.9-3.3-.4-6.7-.8-10-1.3 3.4 .4 6.7 .9 10 1.3 64.1 7.1 133.6-15.1 153.2-80.7 5.9-19.9 15-138.9 15-155.5s-3.3-44.7-21.6-52.9c-15.8-7.1-40-14.9-103.2 29.8-66.1 46.6-137.1 141.1-163.2 191.8z"/>
                 </svg>
+              </a>
+              <a
+                href="mailto:info@kaidev.io"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex text-warmWhite/70 hover:text-kaidevTeal transition-colors flex justify-center items-center"
+                aria-label="BlueSky"
+              >
+                <Mail />&nbsp;info@kaidev.io
               </a>
             </div>
           </div>
