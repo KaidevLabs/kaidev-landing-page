345a743c4be5d05a777d5ac76916d6a7326dd192
Jaime Pascual <jpascual@everlyrusher.com>
2026-07-17 10:22:32 +0200

    New calendar from google (#24)
    * Updated cal link
    
    * bun update

--- components/ui/book-appointment-button.tsx
+++ components/ui/book-appointment-button.tsx
@@ -4,7 +4,7 @@ import { CalendarCheck } from "lucide-react"
 export const BookAppointmentButton = () => {
   return (
     <a
-      href="https://cal.com/kaidev/45min-strategy-call"
+      href="https://calendar.app.google/KxVJzBUsoci682V16"
       target="_blank"
       className="no-underline inline-flex items-center justify-center px-6 py-5
       bg-kaidevTeal text-left text-warmWhite rounded-md font-medium transition-all
@@ -14,7 +14,7 @@ export const BookAppointmentButton = () => {
       <div className="flex flex-col gap-2">
         <div className="flex font-bold inline-block align-middle gap-2">
           <CalendarCheck className="h-6 w-6" />
-          Schedule 45-Min Alignment Call
+          Schedule 30-Min Alignment Call
         </div>
         <div className="text-sm/4">
           This is a complimentary, zero-pressure conversation with our senior strategists.
